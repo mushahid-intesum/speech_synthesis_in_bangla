44dab0f620e5d1134931093ee31e8c87b5158c60
Mushahid Intesum <51900932+mushahid-intesum@users.noreply.github.com>
2024-05-01 15:02:35 +0600

    Update inference.py

--- inference.py
+++ inference.py
@@ -131,7 +131,6 @@ if __name__ == '__main__':
                         params.enc_kernel, params.enc_dropout, params.window_size,
                         params.n_feats, params.dec_dim, params.beta_min, params.beta_max,
                         pe_scale=1000)  # pe_scale=1 for `grad-tts-old.pt`
-    generator.load_state_dict(torch.load('D:\ooga\Thesis\Bangla_TTS\scratch_implementations\Grad-TTS\logs\new_exp_sdp\grad_46.pt', map_location=lambda loc, storage: loc))
     _ = generator.cuda().eval()
     print(f'Number of parameters: {generator.nparams}')
     
